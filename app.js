b7daaaa28e45172cd6e0d4f410ab5d2712db65ec
Suhrob <Suhrob@gmail.com>
2024-12-17 21:52:48 +0500

    t

---
+++ app.js
@@ -0,0 +1,44 @@
+// 1
+let k = 10;
+let n= 5;
+for (let i = 0; i < n; i++ ) {
+    console.count(k);
+}
+// 10
+let n = 8;
+let counter = 0
+for(let i = 1; i < 2 * n - 1;i ++) {
+    counter += 2;
+    sim += counter;
+}
+// 11
+let a = 7;
+let n = 10;
+
+for (let i = 1; i <= n; i++) {
+    console.log(a*= 7);
+}
+// for 1
+let k = 7;
+let n =5;
+for (let i = 0; i < n; i++) {
+    console.log(k)
+}
+//for 2
+let narx = 2400
+for(elt i =1; i <= 10; i++ ) {
+    console.log  (i * narx)
+}
+//for 3
+let n = 5;
+let counter = 0;
+for (let i = 0; i < n; i++) {
+    counter += (n + i ) **2
+}
+//for 4
+let n =5;
+let k = n;
+let counter = 0;
+for(elt i = 1; i <= n; i++) {
+    counter += i ** k
+}
